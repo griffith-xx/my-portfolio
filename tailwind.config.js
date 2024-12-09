2775b90fcb0651014aa03ad8954fcdf6b5a9b3a1
sphinx-p <chacritpoonpipit@gmail.com>
2024-12-09 23:54:39 +0700

    cleaning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 };
